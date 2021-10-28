7c156405713447eeec01e2609463def3cc8d21c8
George Cavazos <cavazosgeorge@yahoo.com>
2021-10-27 22:00:25 -0400

    File structure set | Server Online

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// require your server and launch it
+// require your server and launch it here
+const server = require("./api/server");
+
+const PORT = process.env.PORT;
+
+// START SERVER HERE
+
+server.listen(PORT, () => {
+  console.log(
+    `Server is running on port ${PORT} in ${process.env.NODE_ENV} mode`
+  );
+});
